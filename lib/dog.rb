1f47780f20324c222f9c3c5c0cad6796b6f21ffb
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-02-08 10:24:14 +0300

    Complete Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
+    #attr_accessor :name
+    #attr_accessor :breed
+    
+    #setter method
+    def name=(name)
+        @name = name
+    end
+    #getter method
+    def name
+        @name
+    end
+    #setter method
+    def breed=(breed)
+        @breed = breed
+    end
+    #getter method
+    def breed
+        @breed
+    end
 
 end
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
+    # attr_accessor :name
+    # attr_accessor :job
+
+    #setter method
+    def name=(name)
+        @name = name
+    end
+    #getter method
+    def name
+        @name
+    end
+
+    #setter method
+    def job=(job)
+        @job = job
+    end
+    #getter method
+    def job
+        @job
+    end
 
 end
